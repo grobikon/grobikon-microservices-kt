d2bac33fbae1a243112f724abfe10f5ea8ab8b22
Образцов Григорий <grobikon@mail.ru>
2022-08-07 15:04:10 +0300

    Start convert java code in kotlin.

--- grobikon-users/src/main/java/ru/grobikon/micro/grobikonusers/GrobikonUsersApplication.kt
+++ grobikon-users/src/main/java/ru/grobikon/micro/grobikonusers/GrobikonUsersApplication.kt
@@ -1,21 +1,22 @@
-package ru.grobikon.micro.grobikonusers;
+package ru.grobikon.micro.grobikonusers
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import kotlin.jvm.JvmStatic
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 
 @SpringBootApplication
 @EnableEurekaClient
-@ComponentScan(basePackages = {"ru.grobikon.common", "ru.grobikon.micro.grobikonusers"})
-@EnableJpaRepositories(basePackages = {"ru.grobikon.micro.grobikonusers"})
-@RefreshScope   //динамически читает значения из файла конфигурации
-public class GrobikonUsersApplication {
+@ComponentScan(basePackages = ["ru.grobikon.common", "ru.grobikon.micro.grobikonusers"])
+@EnableJpaRepositories(basePackages = ["ru.grobikon.micro.grobikonusers"])
+@RefreshScope //динамически читает значения из файла конфигурации
+open class GrobikonUsersApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GrobikonUsersApplication.class, args);
+    fun main(args: Array<String>) {
+        runApplication<GrobikonUsersApplication>(*args)
     }
-
 }
